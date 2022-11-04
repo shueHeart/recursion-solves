3f9765b318242ef216b7ebba258d38d2dded99f1
shueHeart <95758078+shueHeart@users.noreply.github.com>
2022-11-04 16:08:18 +0400

    Update DegreeBuilder.java
    fix variables

--- solve_1/DegreeBuilder.java
+++ solve_1/DegreeBuilder.java
@@ -1,17 +1,14 @@
-package recursion;
 
 public class DegreeBuilder {
 	
 	public static long pow(long num, int degree) {
-		
-		long res = num;
-		
+				
 		if (degree > 1)
-			res = num * pow(res, --degree);
+			return num * pow(num, --degree);
 		else if (degree == 0)
 			return 1;
 		
-		return res;
+		return num;
 		
 	}
 	
