9ac3c5b65972c3134654c1938e2868e5ee399d7d
shueHeart <95758078+shueHeart@users.noreply.github.com>
2022-11-09 11:51:58 +0400

    Update QueueLengthCalculator.java
    drop lenght variable

--- solve_3/QueueLengthCalculator.java
+++ solve_3/QueueLengthCalculator.java
@@ -5,14 +5,10 @@ import java.util.Queue;
 public class QueueLengthCalculator {
 	
 	public static int lengthCalculate(Queue<Integer> queue) {
-		
-		int length = 0;
-		
+				
 		if (queue.poll() == null) return 0;
 		
-		++length;
-		
-		return length += lengthCalculate(queue);
+		return 1 + lengthCalculate(queue);
 		
 	}
 	
