84d7aa33fbfb1e591bad154f13f62faf92afa221
shueHeart <95758078+shueHeart@users.noreply.github.com>
2022-11-12 12:22:39 +0400

    Add files via upload
    7 solved

---
+++ solve_7/SecondMaxNumFounder.java
@@ -0,0 +1,23 @@
+package recursion;
+
+import java.util.List;
+
+public class SecondMaxNumFounder {
+	
+	
+	public static int find(List<Integer> list) {
+		return find(list, 0, 0, 0);
+	}
+	
+	private static int find(List<Integer> list, int index, int maxNum, int secondMaxNum) {
+		
+		if (index >= list.size()) return secondMaxNum;
+		
+		if (maxNum <= list.get(index)) {
+			secondMaxNum = maxNum;
+			maxNum = list.get(index);
+		}
+		
+		return find(list, index + 1, maxNum, secondMaxNum);
+	}
+}
